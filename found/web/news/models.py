88b67c2d623055bf7df8ac5f89e7611b264b6678
Ivan Lavriv <ilavriv@admins-Mac-mini.local>
2015-08-23 11:41:59 +0300

    added news models

--- found/web/news/models.py
+++ found/web/news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils.translation import ugettex_lazy as _
 
-# Create your models here.
+
+class Entry(models.Model):
+    title = models.CharField(_('title'), max_length=255)
+    body = models.TextField(_('body'))
+    slug = models.SlugField(_('slug'), max_length=255)
+    attachments = models.FileField()
