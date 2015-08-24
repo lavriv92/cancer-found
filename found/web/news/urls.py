21e052404d94013905382620485057cc44066485
Ivan Lavriv <ilavriv@admins-Mac-mini.local>
2015-08-24 12:39:53 +0300

    Added news detail page

--- found/web/news/urls.py
+++ found/web/news/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$', views.NewsListView.as_view(), name="list"),
+    url(r'^$', views.NewsListView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/$', views.NewsDetailView.as_view(), name='detail')
 ]
--- found/web/news/views.py
+++ found/web/news/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from .models import Entry
 
 
 class NewsListView(ListView):
+    model = Entry
     template_name = 'news/list.html'
-    queryset = set()
+
+
+class NewsDetailView(DetailView):
+    model = Entry
+    context_object_name = 'entry'
+    template_name = 'news/detail.html'
