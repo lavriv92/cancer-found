9f465d6dc03165c1033e108856ce35adff64e98e
Ivan Lavriv <ilavriv@admins-Mac-mini.local>
2015-08-24 03:59:54 +0300

    Updated home page

--- found/web/home/views.py
+++ found/web/home/views.py
@@ -1,8 +1,19 @@
 from django.views.generic import TemplateView
 
+from web.projects.models import Project
+from web.news.models import Entry
+
 
 class HomeView(TemplateView):
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['last_projects'] = Project.objects.all().order_by(
+            '-created'
+        )[:3]
+        context['last_news'] = Entry.objects.all().order_by('-created')[:3]
+        return context
 
 
 class AboutView(TemplateView):
