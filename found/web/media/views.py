c71ea4a3127273a699ec6dd3ee72ba17e8608b72
Ivan Lavriv <ilavriv@admins-Mac-mini.local>
2015-08-30 14:15:27 +0300

    Added media

--- found/web/media/views.py
+++ found/web/media/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import PhotoGroup, VideoGroup
+
+
+class MediaView(TemplateView):
+    templates = 'media/view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MediaView, self).get_context_data(**kwargs)
+        context['video_groups'] = VideoGroup.objects.select_related('videos')
+        context['photo_groups'] = PhotoGroup.objects.select_related('groups')
+        return context
